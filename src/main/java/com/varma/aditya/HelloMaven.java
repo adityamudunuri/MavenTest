1f0cf1157c8e5bcba9883470bc3ec0d9de3b1258
Mudunuri <aditya.v.mudunuri@accenture.com>
2018-08-24 18:34:25 +0530

    first coomit

---
+++ src/main/java/com/varma/aditya/HelloMaven.java
@@ -0,0 +1,10 @@
+package com.varma.aditya;
+
+public class HelloMaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Maven");
+	}
+
+}
